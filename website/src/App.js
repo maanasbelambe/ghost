92b3b85b20da3c8233f06b927061035988a4e148
vivekpoth <vnpothineni@gmail.com>
2024-01-27 12:42:41 -0600

    Update 3

--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './styles/App.css';
-import './styles/BikeAnimation.css'; // Add this line
-import TeamCarousel from './components/TeamCarousel';
+import './styles/BikeAnimation.css';
+//import TeamCarousel from './components/TeamCarousel';
 import GhostHeader from './Ghost_Logo_v2.png';
-import movingBike from './movingBike2.png';
+import { SocialIcon } from 'react-social-icons';
+//import movingBike from './movingBikeTemp.png';
 import TeamMemberCard from './components/TeamMemberCard';
 
 const App = () => {
@@ -19,52 +20,71 @@ const App = () => {
         <nav>
           <ul>
             <li onClick={() => scrollToSection('mission')}>Mission</li>
-            <li onClick={() => scrollToSection('joining')}>Join</li>
             <li onClick={() => scrollToSection('team')}>The Team</li>
             <li onClick={() => scrollToSection('bike')}>The Bike</li>
-            <li onClick={() => scrollToSection('plans')}>Future Plans</li>
+            {/*<li onClick={() => scrollToSection('plans')}>Future Plans</li>*/}
             <li onClick={() => scrollToSection('race')}>The Race</li>
+            <li onClick={() => scrollToSection('contact')}>Contact</li>
+            <li onClick={() => window.open("http://tinyurl.com/3ft4826h", "_blank")} className="join-button">Join Us</li>
           </ul>
         </nav>
       </header>
+      <div className="landing-image">
+        <div className="text-overlay">
+          <h1>Electric Motorcycle Design and Racing{/*cool bike sh!t @ UIUC*/}</h1>
+          <p>
+            at the <span className="highlight">University of Illinois</span>
+          </p>
+        </div>
+      </div>
       <section id="mission">
         <h2>Mission</h2>
-        <p>
-        GHOST Electric is a registered student organization (RSO) at The University of Illinois Urbana-Champaign, whose purpose is to design, construct, and race electric motorcycles while fitting within certain criteria given by the AHRMA Formula Lightning race organizers.
-        </p>
+        <p>The Ghost Electric Motorcycles club at the University of Illinois at Urbana-Champaign is an innovative and dynamic student-run organization with a clear mission: to foster more sustainable engineering processes through the design and construction of electronic motorcycles. </p>
+        <br></br>
+        <p>Ghost aims to participate in the annual American Historical Racing Motorcycle Association (AHRMA) Formula Lightning competition. As a newly established team, Ghost is committed to fostering a vibrant community of aspiring cross-disciplinary engineers, drawing upon the rich expertise and knowledge cultivated within UIUC's prestigious engineering program. With a year-round dedication to their craft, the Ghost team exemplifies a passion for sustainable engineering and a collective pursuit of excellence in the exciting realm of electric motorcycle design and racing.</p>
       </section>
-      <section id="joining">
+      {/*<section id="joining">
         <h2>Interested in Joining?</h2>
-        <p>Fill out this form: <a href="http://tinyurl.com/3ft4826h" target="_blank">http://tinyurl.com/3ft4826h</a></p>
-      </section>
+        <p><a href="http://tinyurl.com/3ft4826h" target="_blank">Click this Form</a></p>
+      </section>*/}
       <section id="team">
         <h2>The Team</h2>
         <div className="team-cards-container">
           {/* Use TeamMemberCard component for each team member */}
           <TeamMemberCard
-            name="Paul Klapperich"
+            firstName="Paul"
+            lastName="Klapperich"
             role="President"
-            bio="Email"
+            bio="Major: Mechanical Engineering"
+            email="paulek2@illinois.edu"
           />
           <TeamMemberCard
-            name="Sam Nutlis"
+            firstName="Sam"
+            lastName="Nutlis"
             role="Vice President"
-            bio="Email"
+            bio="Major: Aerospace Engineering"
+            email="snutlis2@illinois.edu"
           />
           <TeamMemberCard
-            name="Harith Palani"
+            firstName="Harith"
+            lastName="Palani"
             role="Treasurer"
-            bio="Email"
+            bio="Major: Systems Engineering & Design"
+            email="hpala3@illinois.edu"
           />
           <TeamMemberCard
-            name="Sam Blaker"
+            firstName="Sam"
+            lastName="Blaker"
             role="Secretary"
-            bio="Email"
+            bio="Major: Finance in Agribusiness"
+            email="sblaker2@illinois.edu"
           />
           <TeamMemberCard
-            name="Evelina Astranskas"
+            firstName="Evelina"
+            lastName="Astranskas"
             role="Membership Director"
-            bio="Email"
+            bio="Major: Finance + Data Science"
+            email="evelina2@illinois.edu"
           />
           {/* Add more TeamMemberCard components for other team members */}
         </div>
@@ -73,16 +93,28 @@ const App = () => {
         <h2>The Bike</h2>
         <p>Coming Vroom.</p>
       </section>
-      <section id="plans">
+      {/*<section id="plans">
         <h2>Future Plans</h2>
         <p>Text for future plans and goals.</p>
-      </section>
+      </section>*/}
       <section id="race">
         <h2>The Race - AHRMA</h2>
-        <p>The AHRMA Formula Lightning series is the premier zero-emissions motorcycle road racing series in the US. Bikes may be powered by any means of propulsion that does not emit exhaust, such as electricity, magnetism, gravity, flywheels, springs, etc. See more at: <a href="https://ahrma.org/" target="_blank">https://ahrma.org/.</a></p>
+        <p>The AHRMA Formula Lightning series is the premier zero-emissions motorcycle road racing series in the US. Bikes may be powered by any means of propulsion that does not emit exhaust, such as electricity, magnetism, gravity, flywheels, springs, etc. Learn more at <a href="https://ahrma.org/" target="_blank" rel="noopener noreferrer">ahrma.org.</a></p>
+      </section>
+      <section id="contact">
+        <h2>Contact Us</h2>
+        <p>Shoot us an email at <a href={`mailto:ghostuiuc@gmail.com`}> ghostuiuc@gmail.com</a> with any questions or donations!</p>
       </section>
       <footer>
-        <p>&copy; 2024 GHOST Electric. All rights reserved.</p>
+        <p>
+          <SocialIcon url='mailto:ghostuiuc@gmail.com' bgColor='#717f93' fgColor='#ff9f69'/>
+          {' '}
+          <SocialIcon url="https://www.linkedin.com/company/ghost-electric-motorcycle/" bgColor='#ff9f69' fgColor='#717f93'/>
+          {' '}
+          <SocialIcon url="https://www.instagram.com/ghost_uiuc/" bgColor='#717f93' fgColor='#ff9f69'/>
+          {' '}
+          <SocialIcon url="https://discord.gg/56yq68vJKX" bgColor='#ff9f69' fgColor='#717f93'/>
+        </p>
       </footer>
       {/* Add the bike element 
       <img className="bike" src={movingBike} alt="Moving Bike" />*/}
--- website/src/components/TeamMemberCard.js
+++ website/src/components/TeamMemberCard.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TeamMemberCard = ({ name, role, bio,}) => {
+const TeamMemberCard = ({ firstName, lastName, role, bio, email}) => {
   return (
     <div className="team-member-card">
-      <h3>{name}</h3>
+      <h3>{firstName} {lastName}</h3>
       <p>{role}</p>
       <p>{bio}</p>
+      <a href={`mailto:${email}`}> Contact {firstName}</a>
     </div>
   );
 };
 
 TeamMemberCard.propTypes = {
-  name: PropTypes.string.isRequired,
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
   role: PropTypes.string.isRequired,
   bio: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
 };
 
 export default TeamMemberCard;
